6f4308743561d6c4184c4b3ee7444d3578b66f1f
Y-Survivor <yasser.naji@arkx.academy>
2025-03-11 15:09:36 +0000

    Solution of ex2 (day2)

--- week1/day2/ex1.py
+++ week1/day2/ex1.py
@@ -1,3 +1,6 @@
-square = lambda n : n*n
-num = square(5)
-print (num)
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# Using zip to pair the lists and then converting to a dictionary
+convert_2_dict = dict(zip(keys, values))
+print(convert_2_dict)
