e24c17d59ec015d63fa52678674e63026f52a9a8
Y-Survivor <yasser.naji@arkx.academy>
2025-03-11 10:00:17 +0000

    Solution for ex3

---
+++ week1/day1/ex3.py
@@ -0,0 +1,6 @@
+name = "SURVIVOR"
+user_name = input("What is your name? ")
+if user_name.lower() == name.lower():
+    print("Wow, we have the same name! Are you my long-lost twin?")
+else:
+    print(f"Nice name, {user_name}! But let's be honest, {name} is way cooler.")
