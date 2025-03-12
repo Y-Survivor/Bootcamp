45b5579b919a41cd041bd610fb288b83b4a57383
Y-Survivor <yasser.naji@arkx.academy>
2025-03-12 05:11:57 +0000

    Solution for ex4.py

---
+++ week1/day2/ex4.py
@@ -0,0 +1,7 @@
+def describe_city(city, country="Iceland"):
+    print(f"{city} is in {country}.")
+
+# Call the function with different cities and countries
+describe_city("Reykjavik")
+describe_city("Paris", "France")
+describe_city("New York", "USA")
