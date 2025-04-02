eef78be121243c889201f97b3a2eed8f8efc725a
Y-Survivor <yasser.naji@arkx.academy>
2025-04-02 17:21:43 +0000

    Solution of ex2_day1_week4

---
+++ week4/day1/ex2/script.js
@@ -0,0 +1,9 @@
+// 1. Transform the winBattle() function to an arrow function
+const winBattle = () => true;
+
+// 2. Create a variable called experiencePoints
+// 3. Assign a ternary operator to it
+const experiencePoints = winBattle() ? 10 : 1;
+
+// 4. Console.log the experiencePoints variable
+console.log(experiencePoints);
