80af41b6fd2fb83caeee1232d53091f211e34e03
Y-Survivor <yasser.naji@arkx.academy>
2025-04-02 17:43:01 +0000

    Solution for ex4_day1_week4

---
+++ week4/day1/ex4/script.js
@@ -0,0 +1,9 @@
+// Here I created a one line function (ie. an arrow function) that receives two numbers as parameters
+// and returns the sum.
+const sum = (a, b) => a + b;
+
+// Example usage:
+
+console.log(sum(5, 3));    // 8
+console.log(sum(-2, 7));   // 5
+console.log(sum(1.5, 2.5)); // 4
